2ecaf8fd117e9483d60ca7de0b7152cc87da3ad8
VitalyYamaGuchiy <165489241+VitalyYamaGuchiy@users.noreply.github.com>
2024-05-02 15:25:50 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,5 @@
+# Боев Виталий, 15-я когорта — Дипломный проект
+
+URL_SERVICE = " https://e2988140-1cf1-4a60-b32a-7e73cfa72281.serverhub.praktikum-services.ru/"
+CREATE_ORDERS = "/api/v1/orders"
+TRACK_ORDERS = "/api/v1/orders/track"
